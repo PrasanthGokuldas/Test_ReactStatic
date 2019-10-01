83b851c06a683f31114942785e6fc7af7b60f752
prasanth <prasanth@hexosys.com>
2019-10-01 15:28:41 +0800

    remove dynamic page

--- src/App.js
+++ src/App.js
@@ -1,35 +1,30 @@
 import React from 'react'
-import { Root, Routes, addPrefetchExcludes } from 'react-static'
-//
+import { Root, Routes } from 'react-static'
 import { Link, Router } from 'components/Router'
-import Dynamic from 'containers/Dynamic'
 import Header from './pages/common/header';
 import Footer from './pages/common/footer';
 import './app.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-// Any routes that start with 'dynamic' will be treated as non-static routes
-addPrefetchExcludes(['dynamic'])
-
 function App() {
   return (
     <Root>
+      <div className="container">
       <nav>
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
         <Link to="/blog">Blog</Link>
-        <Link to="/dynamic">Dynamic</Link>
       </nav>
       <Header />
       <div className="content">
         <React.Suspense fallback={<em>Loading...</em>}>
           <Router>
-            <Dynamic path="dynamic" />
             <Routes path="*" />
           </Router>
         </React.Suspense>
       </div>
       <Footer />
+      </div>
     </Root>
   )
 }
