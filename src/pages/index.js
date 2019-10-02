4a0ff7e9e03e511fc40d4deb19bc9e5c93ff89f3
prasanth <prasanth@hexosys.com>
2019-10-02 17:13:48 +0800

    font karla added

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,20 @@ import React from 'react'
 
 export default () => (
   <div style={{ textAlign: 'center' }}>
-    <h1>Welcome to React-Static</h1>
+    <h1>Welcome Home</h1>
+    Welcome Home
+    Welcome HomeWelcome Home
+    Welcome Home
+    Welcome HomeWelcome Home
+    Welcome Home
+
+    Welcome HomeWelcome Home
+
+    Welcome Home
+    Welcome Home
+    Welcome Home
+    Welcome Home
+    Welcome Home
+    
   </div>
 )
